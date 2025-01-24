99a6350bf8f41b8ec45f7d96031c352d27c6ab56
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-01-24 12:43:51 +0200

    chore: add web app routing system

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    TanStackRouterVite({
+      routesDirectory: "src/pages",
+    }),
+  ],
+
+  server: {
+    port: 8001,
+  },
+});
