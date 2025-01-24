406b128690f01aebd5ad950cb28813135c770152
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-01-24 11:32:27 +0200

    DELETE: reset project files

--- apps/api/src/utils/auth.ts
+++
@@ -1,7 +0,0 @@
-import { getAuth } from "@hono/clerk-auth";
-import type { Context } from "hono";
-
-export function getUser(c: Context): string | null {
-  const auth = getAuth(c);
-  return auth?.userId || null;
-}
