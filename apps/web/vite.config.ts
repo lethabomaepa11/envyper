badaf6e35362feaeccda5ee8cbb7b09bfa22af0e
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-04-01 08:49:04 +0200

    update vite config

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.API_BASE_URL as string,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+    },
+  };
 });
