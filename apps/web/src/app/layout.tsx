d455c0bedce514965c7823fe37d14be0915d050c
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-04-15 22:05:31 +0200

    add base navbar component

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import "./index.css";
 import Providers from "../context/Providers";
+import Navbar from "@/components/Navbar";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <Providers>
-      <html lang="en">
-        <body>{children}</body>
+      <html lang="en" className="dark">
+        <body>
+          <Navbar />
+          {children}
+        </body>
       </html>
     </Providers>
   );
