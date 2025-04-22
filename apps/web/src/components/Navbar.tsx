45c9a726ebfc5d1796585e6af77b7c3d3154379b
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-04-22 20:10:53 +0200

    add auth paths

--- apps/web/src/components/Navbar.tsx
+++ apps/web/src/components/Navbar.tsx
@@ -61,10 +61,10 @@ export default function App() {
 
       <NavbarContent justify="end">
         <NavbarItem className="hidden lg:flex">
-          <Link href="#">Login</Link>
+          <Link href="/sign-in">Login</Link>
         </NavbarItem>
         <NavbarItem>
-          <Button as={Link} color="primary" href="#" variant="flat">
+          <Button as={Link} color="primary" href="/sign-up" variant="flat">
             Sign Up
           </Button>
         </NavbarItem>
